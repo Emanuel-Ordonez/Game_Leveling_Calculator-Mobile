26ba5a4b7a2a0607a1049c547b33bb4736df5635
jamesbertel <jamesalexanderbertel@gmail.com>
2021-04-05 16:07:29 -0700

    lets hope this works

--- app/src/main/java/com/example/cs481projectapp/cogfacilities/ICogFacility.java
+++ app/src/main/java/com/example/cs481projectapp/cogfacilities/ICogFacility.java
@@ -4,4 +4,5 @@ import java.util.ArrayList;
 
 public interface ICogFacility {
     abstract String calculateStats(int totalObtained, int totalNeeded);
+    abstract String printStats(ArrayList<Integer> nums);
 }
