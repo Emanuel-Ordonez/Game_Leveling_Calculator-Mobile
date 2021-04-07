b4cc1156c1b7e3f9988558b586e9f15de6c46614
jamesbertel <jamesalexanderbertel@gmail.com>
2021-04-07 15:10:07 -0700

    all we need to do now is figure out how to fully stack it

--- app/src/main/java/com/example/cs481projectapp/Calculator.kt
+++ app/src/main/java/com/example/cs481projectapp/Calculator.kt
@@ -5,36 +5,41 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import com.example.cs481projectapp.cogfacilities.*
+import kotlin.reflect.typeOf
 
 class Calculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
 
+        var cogTypeChosen: String = ""
         val cogs = arrayOf("Bossbot", "Lawbot", "Cashbot", "Sellbot")
         val cogTypeSpinner = findViewById<Spinner>(R.id.cogTypeSpinner)
-        cogTypeSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cogs)
-        // someone figure this stuff out:
-    //        cogTypeSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, p3: Long) {
-//                TODO("Not yet implemented")
-//            }
-//        }
 
-        val numPointsNeeded = findViewById<EditText>(R.id.numPointsNeededInput)
-        val numPointsObtained = findViewById<EditText>(R.id.numPointsObtainedInput)
+        cogTypeSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cogs)
+
+        cogTypeSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (view != null) {
+                    //Toast.makeText(view.context, cogTypeSpinner.selectedItem , Toast.LENGTH_LONG).show()
+                    cogTypeChosen = cogTypeSpinner.selectedItem.toString()
+                };
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        val numPointsObtained = findViewById<EditText>(R.id.numPointsObtainedInput).toString()
+        val numPointsNeeded = findViewById<EditText>(R.id.numPointsNeededInput).toString()
         var pointsCalculated = findViewById<TextView>(R.id.statsCalculatedOutput)
         var calculateButton = findViewById<Button>(R.id.calculateButton)
 
-        calculateButton.setOnClickListener {
-
-            TODO("get cogTypeSpinner selection")
-            //pointsCalcuated = findCogCalculation(cogTypeSpinnerSelection, numPointsNeeded, numPointsObtained)
-            //output pointsCalculated (????)
-        }
 
         fun findCogSuit(cog: String, needed: Int, obtained: Int): String{
-
+            TODO("Figure out how to make this work")
             var icogfacility: ICogFacility
             when(cog){
                 "Bossbot" -> icogfacility = BossbotGolfCourse()
@@ -48,6 +53,13 @@ class Calculator : AppCompatActivity() {
                 }
             }
             return icogfacility.calculateStats(obtained, needed)
+        }
+
+        calculateButton.setOnClickListener {
+
+            var obtained = Integer.parseInt(numPointsObtained)
+            var needed = Integer.parseInt(numPointsNeeded)
+//            pointsCalculated.text = findCogSuit(cogTypeChosen, needed, obtained)
         }
     }
 
